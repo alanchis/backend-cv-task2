6ff947cab82f07ddc0d943c178d17abb137a1067
Alan Cruz <alanchis@hotmail.com>
2022-03-02 15:38:28 -0600

    desarrollo backend listo para usuario y mascotas

---
+++ models/Pet.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const petSchema = mongoose.Schema({
+
+	name: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	}
+
+})
+
+const Pet = mongoose.model("Pet", petSchema)
+
+module.exports = Pet
+
+
+
+
